b21f9b6f33d37b92a919f77b216acea24175b66a
Choxmi <gwcsathsara@gmail.com>
2017-07-10 23:01:25 -0700

    FIX DRM-09

--- omod/src/main/java/org/openmrs/module/dhisreport/web/controller/ReportController.java
+++ omod/src/main/java/org/openmrs/module/dhisreport/web/controller/ReportController.java
@@ -444,8 +444,15 @@ public class ReportController
         {
             DataValueType dvtype = new DataValueType();
             dvtype.setDataElement( dv.getDataElement() );
-            dvtype.setValue( new BigDecimal( dv.getValue() ) );
+            try
+            {
+            BigDecimal bd = new BigDecimal( dv.getValue() );
+            dvtype.setValue( bd );
             dvTypeList.add( dvtype );
+            }
+            catch ( NumberFormatException e )
+            {
+            }
         }
         gt.getDataValue().addAll( dvTypeList );
         gt.setOrgUnit( dvs.getOrgUnit() );
